788be1129f17c5c4c3108643d37f131993115a14
Stefan Sayer <stefan.sayer@googlemail.com>
2007-07-10 15:47:54 +0000

    added missing example (why did it not get added in the first place?)
    git-svn-id: http://svn.berlios.de/svnroot/repos/sems/trunk@398 8eb893ce-cfd4-0310-b710-fb5ebe64c474

---
+++ apps/examples/mixin_announce/mix_announce.py
@@ -0,0 +1,25 @@
+
+from log import *
+from ivr import *
+
+class IvrDialog(IvrDialogBase):
+	mixin = None
+	announcement = None
+	beep = None
+
+	def onSessionStart(self, hdrs):
+		debug("onSessionStart of ivr announcement app")
+		self.announcement = IvrAudioFile()
+		self.announcement.open(config['announcement'], ivr.AUDIO_READ)
+		#self.enqueue(self.announcement, None)
+		
+		self.beep = IvrAudioFile()
+	        self.beep.open(config['beep'], ivr.AUDIO_READ)
+		
+		self.mixin = IvrAudioMixIn()
+		self.mixin.init(self.announcement, self.beep, 3, 0.6)
+                self.enqueue(self.mixin, None)
+
+	def onEmptyQueue(self):
+		self.bye()
+		self.stopSession()
