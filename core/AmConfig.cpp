665f9c14734b76c4080be9499fa55ab4d855e641
Raphael Coeffic <rco@iptel.org>
2008-02-22 08:38:35 +0000

    After some researches, it looks like SIGCHLD should never be sent for a dying thread. That's why AmConfig::IgnoreSIGCHLD is now set to true as a default. We may discuss later wether this parameter can/should be removed.
    git-svn-id: http://svn.berlios.de/svnroot/repos/sems/trunk@746 8eb893ce-cfd4-0310-b710-fb5ebe64c474

--- core/AmConfig.cpp
+++ core/AmConfig.cpp
@@ -66,7 +66,7 @@ vector <string> AmConfig::CodecOrder;
 
 Dtmf::InbandDetectorType 
 AmConfig::DefaultDTMFDetector     = Dtmf::SEMSInternal;
-bool AmConfig::IgnoreSIGCHLD  = false;
+bool AmConfig::IgnoreSIGCHLD      = true;
 
 AmSessionTimerConfig AmConfig::defaultSessionTimerConfig;
 
