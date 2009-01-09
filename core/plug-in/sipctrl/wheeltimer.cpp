1e906a0ad4d270caa4601af265d67bc057df1fec
Raphael Coeffic <rco@iptel.org>
2009-01-09 09:55:37 +0000

    - fixes a race condition between timer insertion and deletion. Thx to Peter Loeppky for contributing this path.
    git-svn-id: http://svn.berlios.de/svnroot/repos/sems/trunk@1224 8eb893ce-cfd4-0310-b710-fb5ebe64c474

--- core/plug-in/sipctrl/wheeltimer.cpp
+++ core/plug-in/sipctrl/wheeltimer.cpp
@@ -64,17 +64,17 @@ wheeltimer::~wheeltimer()
 void wheeltimer::insert_timer(timer* t)
 {
     //add new timer to user request list
-    utimer_add_m.lock();
+    utimer_m.lock();
     utimer_add.push(t);
-    utimer_add_m.unlock();
+    utimer_m.unlock();
 }
 
 void wheeltimer::remove_timer(timer* t)
 {
     //add new timer to user request list
-    utimer_rem_m.lock();
+    utimer_m.lock();
     utimer_rem.push(t);
-    utimer_rem_m.unlock();
+    utimer_m.unlock();
 }
 
 void wheeltimer::run()
@@ -155,20 +155,18 @@ void wheeltimer::turn_wheel()
     update_wheel(i);
 	
     // Check for timer insertion requests
-    utimer_add_m.lock();
+    utimer_m.lock();
     while(!utimer_add.empty()) {
 	place_timer(utimer_add.front());
 	utimer_add.pop();
     }
-    utimer_add_m.unlock();
 	
     // Check for timer deletion requests
-    utimer_rem_m.lock();
     while(!utimer_rem.empty()) {
 	delete_timer(utimer_rem.front());
 	utimer_rem.pop();
     }
-    utimer_rem_m.unlock();
+    utimer_m.unlock();
 	
     //check for expired timer to process
     process_current_timers();
