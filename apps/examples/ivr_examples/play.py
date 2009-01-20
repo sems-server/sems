4318da87ebd210459b811a55607228e195a21d1a
Stefan Sayer <stefan.sayer@googlemail.com>
2009-01-20 14:52:58 +0000

    just playing a file example
    git-svn-id: http://svn.berlios.de/svnroot/repos/sems/trunk@1230 8eb893ce-cfd4-0310-b710-fb5ebe64c474

---
+++ apps/examples/ivr_examples/play.py
@@ -0,0 +1,26 @@
+#
+# just playing file
+#
+from log import *
+from ivr import *
+
+class IvrDialog(IvrDialogBase) :
+
+    def onSessionStart( self, hdrs ) :
+        self.audio_pl = IvrAudioFile()
+        self.audio_pl.open("wav/default_en.wav", AUDIO_READ)
+
+        self.enqueue(self.audio_pl,None)
+        
+    def onBye(self):
+        self.stopSession()
+
+    def onEmptyQueue(self):
+        self.bye()
+        self.stopSession()
+
+    def onSipReply( self, reply ) :
+        pass
+
+    def onSipRequest( self, reply ) :
+        pass
