daed7030914b6dea062c69c6c36c2cb5ee3aa9c4
Peter Lemenkov <lemenkov@gmail.com>
2009-10-12 11:06:36 +0000

    - Added CMake configs for apps/twit (still disabled by default).
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>
    
    git-svn-id: http://svn.berlios.de/svnroot/repos/sems/trunk@1527 8eb893ce-cfd4-0310-b710-fb5ebe64c474

---
+++ apps/twit/etc/twit.conf.cmake
@@ -0,0 +1,12 @@
+# url where recordings will be available
+base_url = https://zaraa.iptel.org/twit/
+
+#path to record, should then show up at the above url
+rec_path = /var/www/twits/
+
+# prompts
+welcome_msg = ${SEMS_AUDIO_PREFIX}/sems/audio/twit/twit_welcome.wav
+twit_err_msg = ${SEMS_AUDIO_PREFIX}/sems/audio/twit/twit_error.wav
+twit_account_msg = ${SEMS_AUDIO_PREFIX}/sems/audio/twit/twit_account_msg.wav
+twit_ok_msg = ${SEMS_AUDIO_PREFIX}/sems/audio/twit/twit_ok.wav
+twit_posting_msg = ${SEMS_AUDIO_PREFIX}/sems/audio/twit/twit_posting.wav
