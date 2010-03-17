1b47ae35b9811300e598503ca0eb73afb9495ba2
Stefan Sayer <stefan.sayer@googlemail.com>
2010-03-17 13:07:37 +0000

    removed outdated documentation
    git-svn-id: http://svn.berlios.de/svnroot/repos/sems/trunk@1659 8eb893ce-cfd4-0310-b710-fb5ebe64c474

--- doc/doxyref.h
+++ doc/doxyref.h
@@ -66,27 +66,6 @@
  * 
  */
 
-/*! \page Configure-Sems-Ser-HOWTO mini-Howto on how to configure SER and SEMS to work together
- *
- * <p><b>
- * This is only needed if SER, the SIP Express Router, is used as SIP stack for SEMS. Since 1.0 
- * you can simply use the sipctrl module, which provides a SIP stack for SEMS. sipctrl is set
- * as the default control plugin, so usually there need not be any changes in that regard.</p>
- * <p>But, if you need more functionality from the SIP stack, e.g. TLS, TCP, filtering of inbound 
- * requests etc., you can use ser2 with the binrpcctl module, and the SASI SER application server 
- * interface, and then parts of the information below apply.
- * </p></b>
- * 
- * Be sure to also check the instructions that are on the page with the 
- * <a href="http://www.iptel.org/sems/sems_application_development_tutorial">
- * application development tutorial</a>, there is step by step instructions including 
- * a complete set of configuration files that should get you started with SEMS 
- * very quickly.
- *
- * \verbinclude Configure-Sems-Ser-HOWTO
- * 
- */
-
 /*! \page sems.conf.sample SEMS core configuration parameters
  * <p>The sample configuration file core/etc/sems.conf.sample 
  * explains all core configuration parameters. </p><p>If there is no 
