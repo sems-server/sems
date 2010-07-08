4aadd947667e28f5ce955de4961baed0a109cc7a
Stefan Sayer <stefan.sayer@googlemail.com>
2010-07-09 00:21:26 +0200

    core: fix json parsing of empty arrays and objects

--- core/tests/test_jsonarg.cpp
+++ core/tests/test_jsonarg.cpp
@@ -15,4 +15,38 @@ FCTMF_SUITE_BGN(test_jsonarg) {
       fct_chk(json2arg(s.c_str(), rpc_params));
     } FCT_TEST_END();
 
+    FCT_TEST_BGN(json_empty_struct_parse) {
+      // string s = "{\"jsonrpc\": \"2.0\", \"result\": {\"timestamp\": 62, \"analysis\": {}}, \"id\": \"11\"}";
+      string s = "{\"result\": {}}";
+      AmArg rpc_params;
+      fct_chk(json2arg(s.c_str(), rpc_params));
+      fct_chk(isArgStruct(rpc_params["result"]));
+    } FCT_TEST_END();
+
+    FCT_TEST_BGN(json_empty_array_parse) {
+      string s = "{\"result\": []}";
+      AmArg rpc_params;
+      fct_chk(json2arg(s.c_str(), rpc_params));
+      fct_chk(isArgArray(rpc_params["result"]));
+    } FCT_TEST_END();
+
+    FCT_TEST_BGN(json_error_array_parse) {
+      string s = "{\"result\": [ :1]}";
+      AmArg rpc_params;
+      fct_chk(!json2arg(s.c_str(), rpc_params));
+    } FCT_TEST_END();
+
+    FCT_TEST_BGN(json_error_object_parse) {
+      string s = "{\"result\": { :1}}";
+      AmArg rpc_params;
+      fct_chk(!json2arg(s.c_str(), rpc_params));
+    } FCT_TEST_END();
+
+    FCT_TEST_BGN(json_empty_string_key_parse) {
+      string s = "{\"result\": {\"\" :1}}";
+      AmArg rpc_params;
+      fct_chk(json2arg(s.c_str(), rpc_params));
+      fct_chk(rpc_params["result"][""].asInt()==1);
+    } FCT_TEST_END();
+
 } FCTMF_SUITE_END();
