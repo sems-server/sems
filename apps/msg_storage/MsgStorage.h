7349c5356a1b96dd7a25fe9a50f025a53a108a93
Raphael Coeffic <rco@iptel.org>
2010-09-27 17:29:34 +0200

    Adds notification capabilities to msg_storage.
    The msg_storage plug-in has been extended to send notifications to a AmDynInvoke instance in following cases:
     - new message
     - message marked as read
     - message has been deleted

--- apps/msg_storage/MsgStorage.h
+++ apps/msg_storage/MsgStorage.h
@@ -3,6 +3,9 @@
 
 #include "AmApi.h"
 
+#include <map>
+using std::map;
+
 class MsgStorage : public AmDynInvokeFactory, 
 		   public AmDynInvoke
 {
@@ -11,6 +14,10 @@ class MsgStorage : public AmDynInvokeFactory,
 
   string msg_dir;
 
+  typedef map<AmDynInvoke*,string> Listeners;
+  Listeners  listeners;
+  AmMutex    listeners_mut;
+  
   int msg_new(string domain, string user, string msg_name, FILE* data);
   void msg_get(string domain, string user, string msg_name, AmArg& ret);
   int msg_markread(string domain, string user, string msg_name);
@@ -20,7 +27,15 @@ class MsgStorage : public AmDynInvokeFactory,
   int userdir_close(string domain, string user);
   void userdir_getcount(string domain, string user, AmArg& ret);
 
+  void events_subscribe(AmDynInvoke* event_sink, string method);
+  void events_unsubscribe(AmDynInvoke* event_sink);
+
+  void event_notify(const string& domain, 
+		    const string& user, 
+		    const string& event);
+
   inline void filecopy(FILE* ifp, FILE* ofp);
+
  public:
   MsgStorage(const string& name);
   ~MsgStorage();
