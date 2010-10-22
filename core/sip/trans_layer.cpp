e1e8aa2e6b56880d2db6bddf4eeef723f6b0649a
Stefan Sayer <stefan.sayer@googlemail.com>
2010-10-22 13:34:32 +0200

    warning about missing to-tag only if unhandled

--- core/sip/trans_layer.cpp
+++ core/sip/trans_layer.cpp
@@ -1230,9 +1230,10 @@ int _trans_layer::update_uac_reply(trans_bucket* bucket, sip_trans* t, sip_msg* 
     
     to_tag = ((sip_from_to*)msg->to->p)->tag;
     if((t->msg->u.request->method != sip_request::CANCEL) && !to_tag.len){
-	DBG("To-tag missing in final reply (see sipctrl.conf?)\n");
-	if (!trans_layer::accept_fr_without_totag)
+	if (!trans_layer::accept_fr_without_totag) {
+	    DBG("To-tag missing in final reply (see sems.conf?)\n");
 	    return -1;
+	}
     }
     
     if(t->msg->u.request->method == sip_request::INVITE){
