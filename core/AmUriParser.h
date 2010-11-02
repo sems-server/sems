ec63febf358dbcd8ded0f0550be8c0ca27265975
Stefan Sayer <stefan.sayer@googlemail.com>
2010-11-02 14:44:26 +0100

    compile fix: define AmUriParser only once

--- core/AmUriParser.h
+++ core/AmUriParser.h
@@ -24,6 +24,8 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#ifndef _AmUriParser_h_
+#define _AmUriParser_h_
 
 #include <string>
 #include <map>
@@ -51,3 +53,5 @@ struct AmUriParser {
   void dump();
   AmUriParser() { }
 };
+
+#endif
