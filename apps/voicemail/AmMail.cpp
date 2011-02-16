sems-server/sems

commit 3a6d77830533cc4845f60ae77c9927fbd454139c

use resolver instead of get_ip_from_name.

apps/voicemail/AmMail.cpp
@@ -102,15 +102,9 @@ void AmMailDeamon::run()
     _run_cond.wait_for();
     sleep(5);
 
-    string server_address = get_ip_from_name(AnswerMachineFactory::SmtpServerAddress);
-    if(server_address.empty()){
-      WARN("Mail deamon could not resolv SMTP server address <%s>\n",
-	   AnswerMachineFactory::SmtpServerAddress.c_str());
-      continue;
-    }
-
     AmSmtpClient smtp;
-    if (smtp.connect(server_address,AnswerMachineFactory::SmtpServerPort)) {
+    if (smtp.connect(AnswerMachineFactory::SmtpServerAddress,
+		     AnswerMachineFactory::SmtpServerPort)) {
 	    
       WARN("Mail deamon could not connect to SMTP server at <%s:%i>\n",
 	   server_address.c_str(),AnswerMachineFactory::SmtpServerPort);

apps/voicemail/AmSmtpClient.cpp
@@ -79,15 +79,23 @@ bool AmSmtpClient::connect(const string& _server_ip, unsigned short _server_port
   addr.sin_family = AF_INET;
   addr.sin_port = htons(server_port);
 
-  if(!inet_aton(server_ip.c_str(),&addr.sin_addr)){
-    ERROR("address not valid (smtp server: %s)\n",server_ip.c_str());
-    return -1;
+  {
+    sockaddr_storage _sa;
+    dns_handle       _dh;
+    
+    if(resolver::instance->resolve_name(server_ip.c_str(),
+					&_dh,&_sa,IPv4) < 0) {
+      ERROR("address not valid (smtp server: %s)\n",server_ip.c_str());
+      return false;
+    }
+
+    memcpy(&addr.sin_addr,&((sockaddr_in*)&_sa)->sin_addr,sizeof(in_addr));
   }
 
   sd = socket(PF_INET, SOCK_STREAM, 0);
   if(::connect(sd,(struct sockaddr *)&addr,sizeof(addr)) == -1) {
     ERROR("%s\n",strerror(errno));
-    return -1;
+    return false;
   }
     
   INFO("connected to: %s\n",server_ip.c_str());

commit c199bf8d2cdff939eac41894da82cbe6f137c1a7

b/f: fixes bug introduced in 3a6d778.

apps/voicemail/AmMail.cpp
@@ -107,7 +107,8 @@ void AmMailDeamon::run()
 		     AnswerMachineFactory::SmtpServerPort)) {
 	    
       WARN("Mail deamon could not connect to SMTP server at <%s:%i>\n",
-	   server_address.c_str(),AnswerMachineFactory::SmtpServerPort);
+	   AnswerMachineFactory::SmtpServerAddress.c_str(),
+	   AnswerMachineFactory::SmtpServerPort);
       continue;
     }
 

apps/voicemail/AmSmtpClient.cpp
@@ -38,6 +38,8 @@
 #include "AmUtils.h"
 #include "log.h"
 
+#include "sip/resolver.h"
+
 #define B64_FRAME_LINE_SIZE    15
 #define B64_MAX_BUF_LINES      45
 
@@ -83,7 +85,7 @@ bool AmSmtpClient::connect(const string& _server_ip, unsigned short _server_port
     sockaddr_storage _sa;
     dns_handle       _dh;
     
-    if(resolver::instance->resolve_name(server_ip.c_str(),
+    if(resolver::instance()->resolve_name(server_ip.c_str(),
 					&_dh,&_sa,IPv4) < 0) {
       ERROR("address not valid (smtp server: %s)\n",server_ip.c_str());
       return false;