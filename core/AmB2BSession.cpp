8252e0ca22fc125dd0cb5e0e868b0ade96f8682a
Raphael Coeffic <rco@iptel.org>
2011-04-21 17:47:16 +0200

    allow replies to be processed after clear_other();

--- core/AmB2BSession.cpp
+++ core/AmB2BSession.cpp
@@ -860,15 +860,15 @@ void AmB2BCallerSession::onB2BEvent(B2BEvent* ev)
     AmSipReply& reply = ((B2BSipReplyEvent*)ev)->reply;
 
     if(other_id.empty()){
-      DBG("Discarding B2BSipReply from other leg (other_id empty)\n");
-      DBG("reply code=%i; method=%s; callid=%s; from_tag=%s; "
-	  "to_tag=%s; cseq=%i\n",
+      //DBG("Discarding B2BSipReply from other leg (other_id empty)\n");
+      DBG("B2BSipReply: other_id empty ("
+	  "reply code=%i; method=%s; callid=%s; from_tag=%s; "
+	  "to_tag=%s; cseq=%i)\n",
 	  reply.code,reply.cseq_method.c_str(),reply.callid.c_str(),reply.from_tag.c_str(),
 	  reply.to_tag.c_str(),reply.cseq);
-      return;
+      //return;
     }
-
-    if(other_id != reply.from_tag){// was: local_tag
+    else if(other_id != reply.from_tag){// was: local_tag
       DBG("Dialog mismatch! (oi=%s;ft=%s)\n",
 	  other_id.c_str(),reply.from_tag.c_str());
       return;
