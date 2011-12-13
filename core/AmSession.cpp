eccdce8f778a99166e047ea0490a38ad8787fc3b
Raphael Coeffic <rco@iptel.org>
2011-12-13 17:49:49 +0100

    fixes d4af76b86

--- core/AmSession.cpp
+++ core/AmSession.cpp
@@ -1000,7 +1000,7 @@ bool AmSession::getSdpAnswer(const AmSdp& offer, AmSdp& answer)
       RTPStream()->getSdpAnswer(*m_it,answer_media);
       if(answer_media.payloads.empty() ||
 	 ((answer_media.payloads.size() == 1) &&
-	  (answer_media.payloads[0].encoding_name != "telephone-event"))
+	  (answer_media.payloads[0].encoding_name == "telephone-event"))
 	 ){
 	// no compatible media found
 	throw Exception(488,"no compatible payload");
