sems-server/sems

commit 1645a7d3c6bcf566cd5aa2a8c1a9d3dabaa8dafc

b/f: fixes possible deadlock when messages are sent from handle_sip_reply().
Original patch by Stefan, slightly extended.

core/sip/trans_layer.cpp
@@ -747,9 +747,10 @@ void _trans_layer::timeout(trans_bucket* bucket, sip_trans* t)
     msg.cseq = req->cseq;
     msg.callid = req->callid;
 
-    ua->handle_sip_reply(&msg);
-
     bucket->remove(t);
+    bucket->unlock();
+
+    ua->handle_sip_reply(&msg);
 }
 
 int _trans_layer::send_request(sip_msg* msg, trans_ticket* tt,
@@ -1204,10 +1205,18 @@ void _trans_layer::received_msg(sip_msg* msg)
 	    // Reply matched UAC transaction
 	    
 	    DBG("Reply matched an existing transaction\n");
-	    if(update_uac_reply(bucket,t,msg) < 0){
+	    int res = update_uac_reply(bucket,t,msg);
+	    if(res < 0){
 		ERROR("update_uac_trans() failed, so what happens now???\n");
 		break;
 	    }
+	    if (res) {
+		bucket->unlock();
+		ua->handle_sip_reply(msg);
+		DROP_MSG;
+		//return; - part of DROP_MSG
+	    }
+
 	    // do not touch the transaction anymore:
 	    // it could have been deleted !!!
 	}
@@ -1406,8 +1415,7 @@ int _trans_layer::update_uac_reply(trans_bucket* bucket, sip_trans* t, sip_msg* 
     }
 
  pass_reply:
-    assert(ua);
-    ua->handle_sip_reply(msg);
+    return 1;
  end:
     return 0;
 }
@@ -1648,7 +1656,9 @@ void _trans_layer::timer_expired(timer* t, trans_bucket* bucket, sip_trans* tr)
 	tr->clear_timer(STIMER_B);
 	if(tr->state == TS_CALLING) {
 	    DBG("Transaction timeout!\n");
+	    // unlocks the bucket
 	    timeout(bucket,tr);
+	    return;
 	}
 	else {
 	    DBG("Transaction timeout timer hit while state=0x%x",tr->state);
@@ -1664,8 +1674,9 @@ void _trans_layer::timer_expired(timer* t, trans_bucket* bucket, sip_trans* tr)
 	case TS_TRYING:
 	case TS_PROCEEDING:
 	    DBG("Transaction timeout!\n");
+	    // unlocks the bucket
 	    timeout(bucket,tr);
-	    break;
+	    return;
 	}
 	break;
 
@@ -1751,7 +1762,7 @@ void _trans_layer::timer_expired(timer* t, trans_bucket* bucket, sip_trans* tr)
 	    // get the next ip
 	    if(tr->msg->h_dns.next_ip(&sa) < 0){
 		tr->clear_timer(STIMER_M);
-		return;
+		break;
 	    }
 
 	    //If a SRV record is involved, the port number
@@ -1786,6 +1797,8 @@ void _trans_layer::timer_expired(timer* t, trans_bucket* bucket, sip_trans* tr)
 	ERROR("Invalid timer type %i\n",t->type);
 	break;
     }
+
+    bucket->unlock();
 }
 
 /**

core/sip/sip_trans.cpp
@@ -154,13 +154,15 @@ void trans_timer_cb(timer* t, unsigned int bucket_id, sip_trans* tr)
 	if(bucket->exist(tr)){
 	    DBG("Transaction timer expired: type=%c, trans=%p, eta=%i, t=%i\n",
 		timer_name(t->type),tr,t->expires,wheeltimer::instance()->wall_clock);
+
+	    // timer_expired unlocks the bucket
 	    trans_layer::instance()->timer_expired(t,bucket,tr);
 	}
 	else {
 	    WARN("Ignoring expired timer (%p): transaction"
 		 " %p does not exist anymore\n",t,tr);
+	    bucket->unlock();
 	}
-	bucket->unlock();
     }
     else {
 	ERROR("Invalid bucket id\n");

commit 3fe1dde8e066024e2f15ef094f23dba9199508b0

b/f: fixes timeout issue introduced in 1645a7d3c6.

core/sip/trans_layer.cpp
@@ -727,14 +727,32 @@ int _trans_layer::set_destination_ip(sip_msg* msg, cstring* next_hop, unsigned s
     return 0;
 }
 
+static void translate_string(sip_msg* dst_msg, cstring& dst,
+			     sip_msg* src_msg, cstring& src)
+{
+    dst.s = (char*)src.s + (dst_msg->buf - src_msg->buf);
+    dst.len = src.len;
+}
+
+static void translate_hdr(sip_msg* dst_msg, sip_header*& dst, 
+			  sip_msg* src_msg, sip_header* src)
+{
+    dst = new sip_header();
+    dst_msg->hdrs.push_back(dst);
+    dst->type = src->type;
+    translate_string(dst_msg,dst->name,src_msg,src->name);
+    translate_string(dst_msg,dst->value,src_msg,src->value);
+    dst->p = NULL;
+}
+
 void _trans_layer::timeout(trans_bucket* bucket, sip_trans* t)
 {
     t->reset_all_timers();
     t->state = TS_TERMINATED;
 
     // send 408 to 'ua'
-    sip_msg  msg;
     sip_msg* req = t->msg;
+    sip_msg  msg(req->buf,req->len);
 
     msg.type = SIP_REPLY;
     msg.u.reply = new sip_reply();
@@ -742,10 +760,22 @@ void _trans_layer::timeout(trans_bucket* bucket, sip_trans* t)
     msg.u.reply->code = 408;
     msg.u.reply->reason = cstring("Timeout");
 
-    msg.from = req->from;
-    msg.to = req->to;
-    msg.cseq = req->cseq;
-    msg.callid = req->callid;
+    translate_hdr(&msg,msg.from, req,req->from);
+    msg.from->p = new sip_from_to();
+    parse_from_to((sip_from_to*)msg.from->p,
+		  msg.from->value.s,msg.from->value.len);
+
+    translate_hdr(&msg,msg.to, req,req->to);
+    msg.to->p = new sip_from_to();
+    parse_from_to((sip_from_to*)msg.to->p,
+		  msg.to->value.s,msg.to->value.len);
+
+    translate_hdr(&msg,msg.cseq, req,req->cseq);
+    msg.cseq->p = new sip_cseq();
+    parse_cseq((sip_cseq*)msg.cseq->p,
+	       msg.cseq->value.s,msg.cseq->value.len);
+
+    translate_hdr(&msg,msg.callid, req,req->callid);
 
     bucket->remove(t);
     bucket->unlock();