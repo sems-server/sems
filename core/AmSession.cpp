a7516d000029195706c198e5f3373cee16729249
Stefan Sayer <stefan.sayer@googlemail.com>
2012-03-12 16:48:22 +0100

    b/f: on RTP timeout, end dialog with sending BYE

--- core/AmSession.cpp
+++ core/AmSession.cpp
@@ -1106,7 +1106,8 @@ void AmSession::onSendReply(const AmSipRequest& req, unsigned int  code,
 
 void AmSession::onRtpTimeout()
 {
-  DBG("stopping Session.\n");
+  DBG("RTP timeout, stopping Session\n");
+  dlg.bye();
   setStopped();
 }
 
