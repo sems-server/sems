a7d70aa1a7883a2cd4c7e1b34f863177014512ef
Stefan Sayer <stefan.sayer@googlemail.com>
2012-04-15 23:24:12 +0200

    DSM: c/f fixing fdf221d74a5b48ce589953d03b2922bccd6bc34e

--- apps/dsm/DSMCall.cpp
+++ apps/dsm/DSMCall.cpp
@@ -100,7 +100,7 @@ void DSMCall::onInvite(const AmSipRequest& req) {
     
   bool run_session_invite = engine.onInvite(req, this);
 
-  avar[DSM_AVAR_REQUEST] = AmArg(sip_req);
+  avar[DSM_AVAR_REQUEST] = AmArg(&req);
 
   DBG("before runEvent(this, this, DSMCondition::Invite);\n");
   AmSipDialog::Status old_st = dlg.getStatus();
