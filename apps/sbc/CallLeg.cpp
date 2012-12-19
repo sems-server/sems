sems-server/sems

commit b987d4c48c506b36761ef9daa6a1611335f5becd

optimization: save SDP only if refresh_method!=UPD

core/AmB2BSession.cpp
@@ -407,7 +407,8 @@ void AmB2BSession::relaySip(const AmSipRequest& req)
     dlg.sendRequest(req.method, req.content_type, req.body, *hdrs, SIP_FLAGS_VERBATIM);
     // todo: relay error event back if sending fails
 
-    if ((req.method == SIP_METH_INVITE ||
+    if ((refresh_method != REFRESH_UPDATE) &&
+	(req.method == SIP_METH_INVITE ||
 	 req.method == SIP_METH_UPDATE) &&
 	!req.body.empty()) {
       saveSessionDescription(req.content_type, req.body);
@@ -431,7 +432,9 @@ void AmB2BSession::relaySip(const AmSipRequest& req)
     dlg.send_200_ack(AmSipTransaction(t->second.method, t->first,t->second.tt), 
 		     req.content_type, req.body, req.hdrs, SIP_FLAGS_VERBATIM);
 
-    if (!req.body.empty() && t->second.method == SIP_METH_INVITE) {
+    if ((refresh_method != REFRESH_UPDATE) &&
+	!req.body.empty() &&
+	(t->second.method == SIP_METH_INVITE)) {
     // delayed SDP negotiation - save SDP
       saveSessionDescription(req.content_type, req.body);
     }
@@ -455,7 +458,8 @@ void AmB2BSession::relaySip(const AmSipRequest& orig, const AmSipReply& reply)
 	    reply.content_type,
 	    reply.body, *hdrs,SIP_FLAGS_VERBATIM);
 
-  if ((orig.method == SIP_METH_INVITE ||
+  if ((refresh_method != REFRESH_UPDATE) &&
+      (orig.method == SIP_METH_INVITE ||
        orig.method == SIP_METH_UPDATE) &&
       !reply.body.empty()) {
     saveSessionDescription(reply.content_type, reply.body);
@@ -744,7 +748,8 @@ void AmB2BCalleeSession::onB2BEvent(B2BEvent* ev)
       return;
     }
 
-    saveSessionDescription(co_ev->content_type, co_ev->body);
+    if (refresh_method != REFRESH_UPDATE)
+      saveSessionDescription(co_ev->content_type, co_ev->body);
 
     return;
   }    

commit f70d8303818e2f28f0c9bc80c0660decc4456aa4

B2B calls b/f: saving SDP regardless of refresh_method settings
breaks optimization introduced by b987d4c4 but saved SDP is needed for
successful reINVITEs regardless of session refreshes

apps/sbc/CallLeg.cpp
@@ -449,8 +449,9 @@ void CallLeg::onB2BConnect(ConnectLegEvent* co_ev)
   }
   else est_invite_other_cseq = 0;
 
-  if (refresh_method != REFRESH_UPDATE)
+  if (!co_ev->body.empty()) {
     saveSessionDescription(co_ev->body);
+  }
 
   // save CSeq of establising INVITE
   est_invite_cseq = dlg.cseq - 1;
@@ -527,7 +528,7 @@ void CallLeg::onB2BReconnect(ReconnectLegEvent* ev)
   }
   else est_invite_other_cseq = 0;
 
-  if (refresh_method != REFRESH_UPDATE) saveSessionDescription(ev->body);
+  saveSessionDescription(ev->body);
 
   // save CSeq of establising INVITE
   est_invite_cseq = dlg.cseq - 1;

core/AmB2BSession.cpp
@@ -732,8 +732,7 @@ int AmB2BSession::relaySip(const AmSipRequest& req)
       return err;
     }
 
-    if ((refresh_method != REFRESH_UPDATE) &&
-	(req.method == SIP_METH_INVITE ||
+    if ((req.method == SIP_METH_INVITE ||
 	 req.method == SIP_METH_UPDATE) &&
 	!req.body.empty()) {
       saveSessionDescription(req.body);
@@ -761,8 +760,7 @@ int AmB2BSession::relaySip(const AmSipRequest& req)
       return err;
     }
 
-    if ((refresh_method != REFRESH_UPDATE) &&
-	!req.body.empty() &&
+    if (!req.body.empty() &&
 	(t->second.method == SIP_METH_INVITE)) {
     // delayed SDP negotiation - save SDP
       saveSessionDescription(req.body);
@@ -808,8 +806,7 @@ int AmB2BSession::relaySip(const AmSipRequest& orig, const AmSipReply& reply)
     return err;
   }
 
-  if ((refresh_method != REFRESH_UPDATE) &&
-      (orig.method == SIP_METH_INVITE ||
+  if ((orig.method == SIP_METH_INVITE ||
        orig.method == SIP_METH_UPDATE) &&
       !reply.body.empty()) {
     saveSessionDescription(reply.body);
@@ -1219,8 +1216,7 @@ void AmB2BCalleeSession::onB2BEvent(B2BEvent* ev)
       return;
     }
 
-    if (refresh_method != REFRESH_UPDATE)
-      saveSessionDescription(co_ev->body);
+    saveSessionDescription(co_ev->body);
 
     // save CSeq of establising INVITE
     est_invite_cseq = dlg.cseq - 1;