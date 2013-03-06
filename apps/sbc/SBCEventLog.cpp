f4fb79bfad7b498f22101bdf54fa1110aefb0b5d
Raphael Coeffic <rco@iptel.org>
2013-03-06 17:17:05 +0100

    sbc: added event log

---
+++ apps/sbc/SBCEventLog.cpp
@@ -0,0 +1,49 @@
+#include "SBCEventLog.h"
+#include "AmAppTimer.h"
+
+#include "AmArg.h"
+#include "ampi/MonitoringAPI.h"
+#include "AmSessionContainer.h" // monitoring ptr
+
+struct MonitoringEventLogHandler
+  : public SBCEventLogHandler
+{
+  void logEvent(long int timestamp, const string& id, const AmArg& event) {
+    if(NULL != MONITORING_GLOBAL_INTERFACE) {
+      AmArg di_args,ret;
+      di_args.push(id);
+      di_args.push("ts");
+      di_args.push(timestamp);
+      di_args.push("attrs");
+      di_args.push(event);
+
+      MONITORING_GLOBAL_INTERFACE->
+	invoke("log", di_args, ret);
+    }
+  }
+};
+
+void _SBCEventLog::useMonitoringLog()
+{
+  if(NULL != MONITORING_GLOBAL_INTERFACE) {
+    setEventLogHandler(new MonitoringEventLogHandler());
+    INFO("SBC event log will use the monitoring module\n");
+  }
+  else {
+    ERROR("SBC event log cannot use the monitoring module"
+	  " as it is not loaded\n");
+  }
+}
+
+void _SBCEventLog::setEventLogHandler(SBCEventLogHandler* lh)
+{
+  log_handler.reset(lh);
+}
+
+void _SBCEventLog::logEvent(const string& id, const AmArg& event)
+{
+  if(log_handler.get()) {
+    log_handler->logEvent(AmAppTimer::instance()->unix_clock.get(),
+			  id, event);
+  }
+}
---
+++ apps/sbc/SBCEventLog.h
@@ -0,0 +1,36 @@
+#ifndef _SBCEventLog_h_
+#define _SBCEventLog_h_
+
+#include "singleton.h"
+#include "AmArg.h"
+
+#include <memory>
+#include <string>
+#include <map>
+using std::auto_ptr;
+using std::string;
+using std::map;
+
+struct SBCEventLogHandler
+{
+  virtual void logEvent(long int timestamp, const string& id, 
+			const AmArg& ev)=0;
+};
+
+class _SBCEventLog
+{
+  auto_ptr<SBCEventLogHandler> log_handler;
+
+protected:
+  _SBCEventLog() {}
+  ~_SBCEventLog() {}
+
+public:
+  void useMonitoringLog();
+  void setEventLogHandler(SBCEventLogHandler* lh);
+  void logEvent(const string& id, const AmArg& event);
+};
+
+typedef singleton<_SBCEventLog> SBCEventLog;
+
+#endif
