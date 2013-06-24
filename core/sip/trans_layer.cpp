sems-server/sems

commit eb80fbdac0134f68bfddd809374b69b3241607ef

add some more support for transports other than UDP.

core/sip/trans_layer.cpp
@@ -709,8 +709,10 @@ static void prepare_strict_routing(sip_msg* msg, string& ext_uri_buffer)
 //
 int _trans_layer::set_next_hop(sip_msg* msg, 
 			       cstring* next_hop,
-			       unsigned short* next_port)
+			       unsigned short* next_port,
+			       cstring* next_trsp)
 {
+    static const cstring default_trsp("udp");
     assert(msg);
 
     list<sip_header*>& route_hdrs = msg->route; 
@@ -730,6 +732,8 @@ int _trans_layer::set_next_hop(sip_msg* msg,
 	    *next_hop  = route_uri->host;
 	    if(route_uri->port_str.len)
 		*next_port = route_uri->port;
+	    if(route_uri->trsp->value.len)
+		*next_trsp = route_uri->trsp->value;
 	}
     }
     else {
@@ -742,12 +746,21 @@ int _trans_layer::set_next_hop(sip_msg* msg,
 	    ERROR("Invalid Request URI\n");
 	    return -1;
 	}
+	DBG("setting next-hop based on request-URI\n");
 	*next_hop  = parsed_r_uri.host;
 	if(parsed_r_uri.port_str.len)
 	    *next_port = parsed_r_uri.port;
+	if(parsed_r_uri.trsp)
+	    *next_trsp = parsed_r_uri.trsp->value;
     }
 
-    DBG("next_hop:next_port is <%.*s:%u>\n", next_hop->len, next_hop->s, *next_port);
+    if(!next_trsp->len)
+	*next_trsp = default_trsp;
+
+    DBG("next_hop:next_port is <%.*s:%u/%.*s>\n",
+	next_hop->len, next_hop->s, *next_port,
+	next_trsp ? next_trsp->len : 0,
+	next_trsp ? next_trsp->s : 0);
     
     return 0;
 }
@@ -883,6 +896,7 @@ int _trans_layer::send_request(sip_msg* msg, trans_ticket* tt,
 
     cstring next_hop;
     unsigned short next_port=0;
+    cstring next_trsp;
 
     int res=0;
     if (_next_hop.len) {
@@ -895,8 +909,13 @@ int _trans_layer::send_request(sip_msg* msg, trans_ticket* tt,
 	}
 
 	if(dest_list.size() == 1) {
-	    next_hop = dest_list.front().host;
-	    next_port = dest_list.front().port ? dest_list.front().port : 5060;
+	    const sip_destination& dest = dest_list.front();
+	    next_hop = dest.host;
+	    next_port = dest.port ? dest.port : 5060;
+	    next_trsp = dest.trsp;
+	    DBG("single next-hop: <%.*s:%u/%.*s>",
+		next_hop.len,next_hop.s,next_port,
+		next_trsp.len,next_trsp.s);
 	}
 	else if(dest_list.size() > 1) {
 	    dns_ip_entry* e = new dns_ip_entry();
@@ -906,12 +925,13 @@ int _trans_layer::send_request(sip_msg* msg, trans_ticket* tt,
 	    }
 
 	    inc_ref(e);
+	    //TODO: avoid to loose the transport from the next-hop-list
 	    e->next_ip(&msg->h_dns,&msg->remote_ip);
 	    DBG("destination set to <%s>\n",get_addr_str(&msg->remote_ip).c_str());
 	}
     }
     else {
-	if(set_next_hop(msg,&next_hop,&next_port) < 0){
+	if(set_next_hop(msg,&next_hop,&next_port,&next_trsp) < 0){
 	    DBG("set_next_hop failed\n");
 	    return -1;
 	}
@@ -1679,24 +1699,36 @@ int _trans_layer::update_uac_request(trans_bucket* bucket, sip_trans*& t, sip_ms
 	return 0;
     }
 
+    DBG("transport = '%s'; is_reliable = %i",
+	msg->local_socket->get_transport(),
+	msg->local_socket->is_reliable());
+
     switch(msg->u.request->method){
 
     case sip_request::INVITE:
-	// if transport == UDP
-	t->reset_timer(STIMER_A,A_TIMER,bucket->get_id());
-	// for any transport type
-	t->reset_timer(STIMER_B,B_TIMER,bucket->get_id());
+	if(!msg->local_socket->is_reliable()) {
+	    // if transport == UDP
+	    t->reset_timer(STIMER_A,A_TIMER,bucket->get_id());
+	}
+	else {
+	    // for any transport type
+	    t->reset_timer(STIMER_B,B_TIMER,bucket->get_id());
+	}
 	break;
     
     default:
-	// if transport == UDP
-	t->reset_timer(STIMER_E,E_TIMER,bucket->get_id());
-	// for any transport type
-	t->reset_timer(STIMER_F,F_TIMER,bucket->get_id());
+	if(!msg->local_socket->is_reliable()) {
+	    // if transport == UDP
+	    t->reset_timer(STIMER_E,E_TIMER,bucket->get_id());
+	}
+	else {
+	    // for any transport type
+	    t->reset_timer(STIMER_F,F_TIMER,bucket->get_id());
+	}
 	break;
     }
 
-    if(!msg->h_dns.eoip()){ // if transport == UDP
+    if(!msg->h_dns.eoip()){
 	t->reset_timer(STIMER_M,M_TIMER,bucket->get_id());
     }
 
@@ -2140,9 +2172,11 @@ int _trans_layer::try_next_ip(trans_bucket* bucket, sip_trans* tr)
 			tr->msg->u.request->method_str);
     }
 
-    // reset counter for timer A & E
-    trans_timer* A_E_timer = tr->get_timer(STIMER_A);
-    tr->reset_timer(A_E_timer->type & 0xFFFF,A_TIMER,bucket->get_id());
+    if(!tr->msg->local_socket->is_reliable()) {
+	// reset counter for timer A & E
+	trans_timer* A_E_timer = tr->get_timer(STIMER_A);
+	tr->reset_timer(A_E_timer->type & 0xFFFF,A_TIMER,bucket->get_id());
+    }
     
     if(!tr->msg->h_dns.eoip())
 	tr->reset_timer(STIMER_M,M_TIMER,bucket->get_id());

apps/sbc/RegisterCache.cpp
@@ -1089,6 +1089,7 @@ bool _RegisterCache::saveSingleContact(RegisterCacheCtx& ctx,
   alias_update.source_ip = req.remote_ip;
   alias_update.source_port = req.remote_port;
   alias_update.remote_ua = getHeader(req.hdrs,"User-Agent");
+  alias_update.trsp = req.trsp;
   alias_update.local_if = req.local_if;
   alias_update.ua_expire = ua_expires + now.tv_sec;
 

apps/sbc/RegisterCache.h
@@ -56,6 +56,7 @@ struct AliasEntry
   // saved state for NAT handling
   string         source_ip;
   unsigned short source_port;
+  string         trsp;
 
   // sticky interface
   unsigned short local_if;

apps/sbc/RegisterDialog.cpp
@@ -366,6 +366,7 @@ int RegisterDialog::initUAC(const AmSipRequest& req, const SBCCallProfile& cp)
     source_port = req.remote_port;
     local_if = req.local_if;
     from_ua = getHeader(req.hdrs,"User-Agent");
+    transport = req.trsp;
 
     min_reg_expire = cp.min_reg_expires;
     max_ua_expire = cp.max_ua_expires;
@@ -498,6 +499,7 @@ void RegisterDialog::onSipReply(const AmSipRequest& req,
 	alias_entry.source_ip   = source_ip;
 	alias_entry.source_port = source_port;
 	alias_entry.remote_ua   = from_ua;
+	alias_entry.trsp        = transport;
 	alias_entry.local_if    = local_if;
 	alias_entry.ua_expire   = orig_expires + now.tv_sec;
 

apps/sbc/RegisterDialog.h
@@ -18,6 +18,7 @@ class RegisterDialog
   string         aor; // From-URI
   string         source_ip;
   unsigned short source_port;
+  string         transport;
   unsigned short local_if;
   string         from_ua;
 

core/AmSipMsg.h
@@ -41,6 +41,7 @@ class _AmSipMsgInDlg
   unsigned short remote_port;
   string         local_ip;
   unsigned short local_port;
+  string         trsp;
 
   _AmSipMsgInDlg() : cseq(0), rseq(0) { }
   virtual ~_AmSipMsgInDlg() { };

core/SipCtrlInterface.cpp
@@ -532,6 +532,7 @@ inline bool _SipCtrlInterface::sip_msg2am_request(const sip_msg *msg,
     req.local_ip = get_addr_str(&msg->local_ip).c_str();
     req.local_port = htons(((sockaddr_in*)&msg->local_ip)->sin_port);
 
+    req.trsp = msg->local_socket->get_transport();
     req.local_if = msg->local_socket->get_if();
 
     if(msg->vias.size() > 1) {
@@ -641,8 +642,8 @@ void _SipCtrlInterface::handle_sip_request(const trans_ticket& tt, sip_msg* msg)
     if(!sip_msg2am_request(msg, tt, req))
 	return;
 
-    DBG("Received new request from <%s:%i> on intf #%i\n",
-	req.remote_ip.c_str(),req.remote_port,req.local_if);
+    DBG("Received new request from <%s:%i/%s> on intf #%i\n",
+	req.remote_ip.c_str(),req.remote_port,req.trsp.c_str(),req.local_if);
 
     if (_SipCtrlInterface::log_parsed_messages) {
 	//     DBG_PARAM(req.cmd);

core/sip/parse_next_hop.cpp
@@ -146,6 +146,10 @@ int parse_next_hop(const cstring& next_hop,
 	dest_list.push_back(dest);
 	break;
       default:
+	if( (*c >= 'a' && *c <= 'z') ||
+	    (*c >= 'A' && *c <= 'Z') ) {
+	  continue;
+	}
 	// syntax error
 	DBG("error: unexpected character '%c' in IPL_TRSP state.\n",*c);
 	return -1;
@@ -161,9 +165,15 @@ int parse_next_hop(const cstring& next_hop,
     break;
   case IPL_HOST:
     dest.host.set(beg,c-beg);
+    dest_list.push_back(dest);
+    break;
   case IPL_V6:
   case IPL_HOST_SEP:
   case IPL_PORT:
+    dest_list.push_back(dest);
+    break;
+  case IPL_TRSP:
+    dest.trsp.set(beg,c-beg);
     dest_list.push_back(dest);
     break;
   }

commit 6118549a62712d615aad886c0454b2b9af4f5343

sip: critical fix for final reply timers (INV & non-INV transaction)
the transaction layer would have only set the retransmission timer and no final reply timer in case of non-reliable transports. This bug has been introduced by commit eb80fbdac01.

core/sip/trans_layer.cpp
@@ -1778,10 +1778,9 @@ int _trans_layer::update_uac_request(trans_bucket* bucket, sip_trans*& t, sip_ms
 	    // if transport == UDP
 	    t->reset_timer(STIMER_A,A_TIMER,bucket->get_id());
 	}
-	else {
-	    // for any transport type
-	    t->reset_timer(STIMER_B,B_TIMER,bucket->get_id());
-	}
+
+	// for any transport type
+	t->reset_timer(STIMER_B,B_TIMER,bucket->get_id());
 	break;
     
     default:
@@ -1789,10 +1788,9 @@ int _trans_layer::update_uac_request(trans_bucket* bucket, sip_trans*& t, sip_ms
 	    // if transport == UDP
 	    t->reset_timer(STIMER_E,E_TIMER,bucket->get_id());
 	}
-	else {
-	    // for any transport type
-	    t->reset_timer(STIMER_F,F_TIMER,bucket->get_id());
-	}
+
+	// for any transport type
+	t->reset_timer(STIMER_F,F_TIMER,bucket->get_id());
 	break;
     }
 