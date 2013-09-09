183aff6da3a41f6194d49db095dc069ecfced626
Václav Kubart <vaclav.kubart@frafos.com>
2013-09-09 09:03:59 +0200

    uri parser: parse headers behind URI parameters

--- core/AmUriParser.cpp
+++ core/AmUriParser.cpp
@@ -283,6 +283,8 @@ bool AmUriParser::parse_uri() {
       switch (c) {
       case '>': { uri_param = uri.substr(p1+1, pos-p1-1);
       st = uS6; p1 = pos; } break;
+      case '?': { uri_param = uri.substr(p1+1, pos-p1-1);
+      st = uSHDR; p1 = pos; } break;
       case '\t':
       case ' ': { uri_param = uri.substr(p1+1, pos-p1-1);
       st = uSPARAMWSP; p1 = pos; } break;      
