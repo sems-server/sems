43aab77c7150708f638fe76b009b6740904f9569
Stefan Sayer <stefan.sayer@googlemail.com>
2013-12-21 19:23:38 +0100

    sbc:dsm: next_hop options for addCallee with new call mode

--- apps/sbc/call_control/dsm/SBCDSMParams.h
+++ apps/sbc/call_control/dsm/SBCDSMParams.h
@@ -18,6 +18,11 @@
 #define DSM_SBC_PARAM_ADDCALLEE_REMOTE_PARTY "remote_party"
 #define DSM_SBC_PARAM_ADDCALLEE_HDRS           "hdrs"
 #define DSM_SBC_PARAM_ADDCALLEE_OUTBOUND_PROXY "outbound_proxy"
+#define DSM_SBC_PARAM_ADDCALLEE_NEXT_HOP         "next_hop"
+#define DSM_SBC_PARAM_ADDCALLEE_NEXT_HOP_1ST_REQ "next_hop_1st_req"
+#define DSM_SBC_PARAM_ADDCALLEE_NEXT_HOP_PATCH_RURI "next_hop_patch_ruri"
+#define DSM_SBC_PARAM_ADDCALLEE_NEXT_HOP_FIXED      "next_hop_fixed"
+
 #define DSM_SBC_PARAM_ADDCALLEE_RTP_MODE       "rtp_mode"
 #define DSM_SBC_PARAM_ADDCALLEE_TRANSPARENT_DLG_ID "transparent_dlg_id"
 
