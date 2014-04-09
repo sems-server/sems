7eb8e79d5315c578f13eb0d85589ae16768306fb
Václav Kubart <vaclav.kubart@frafos.com>
2014-04-09 10:18:18 +0200

    sbc b/f: allow to disable session timers in A leg
    thanks Herve Couplet for reporting and providing the fix

--- apps/sbc/SBCCallLeg.cpp
+++ apps/sbc/SBCCallLeg.cpp
@@ -794,7 +794,7 @@ void SBCCallLeg::onInvite(const AmSipRequest& req)
   call_profile.sst_enabled = ctx.replaceParameters(call_profile.sst_enabled, 
 						   "enable_session_timer", req);
 
-  if ((call_profile.sst_aleg_enabled == "yes") ||
+  if ((call_profile.sst_aleg_enabled == "yes") &&
       (call_profile.sst_enabled == "yes")) {
 
     call_profile.eval_sst_config(ctx,req,call_profile.sst_a_cfg);
