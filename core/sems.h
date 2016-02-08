8b997d8043f0ee3d5d31f28e02c9688419898247
Mikko Lehto <mslehto@iki.fi>
2016-02-08 05:17:48 +0200

    core: fix include guard name

--- core/sems.h
+++ core/sems.h
@@ -25,8 +25,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 /** @file sems.h */
-#ifndef _ans_machine_h_
-#define _ans_machine_h_
+#ifndef _sems_h_
+#define _sems_h_
 
 #define CONFIG_FILE         "/usr/local/etc/sems/sems.conf"
 #define MOD_CFG_PATH        "/usr/local/etc/sems/etc/"
