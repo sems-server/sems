sems-server/sems

commit d50b51821706f6e86547cced03bd78119ee1da61

Makefiles: replaced illegal - chars in exported variable names with _ chars - removed two unused exported variables

apps/ivr/Makefile.ivr_application
@@ -10,9 +10,9 @@ include $(COREPATH)/../Makefile.defs
 include $(IVRPATH)/Makefile.defs
 
 # 
-ivr-modules-dir?=lib/$(APP_NAME)/ivr
+ivr-modules_dir?=lib/$(APP_NAME)/ivr
 
-LIB_INSTALLDIR?=$(modules-prefix)/$(ivr-modules-dir)
+LIB_INSTALLDIR?=$(modules_prefix)/$(ivr-modules_dir)
 TARBALL_PREFIX=sems-app-${NAME}
 TARBALL="${TARBALL_PREFIX}-${VERSION}.tar.gz"
 
@@ -50,26 +50,26 @@ endif
 
 .PHONY: install-cfg
 install-cfg: 
-	mkdir -p $(DESTDIR)$(app-cfg-target)
+	mkdir -p $(DESTDIR)$(app_cfg_target)
 	-@for r in $(module_conf_files); do \
 			echo installing $$r ;  \
-			$(INSTALL-TOUCH) $(DESTDIR)$(app-cfg-target)$$r ; \
-			sed -e "s#/usr/.*lib/sems/audio/#$(audio-target)#g" \
-			-e "s#/usr/.*lib/sems/plug-in/#$(modules-target)#g" \
-			-e "s#/usr/.*etc/sems/#$(app-cfg-target)#g" \
-			< etc/$$r > $(DESTDIR)$(app-cfg-target)$$r; \
+			$(INSTALL_TOUCH) $(DESTDIR)$(app_cfg_target)$$r ; \
+			sed -e "s#/usr/.*lib/sems/audio/#$(audio_target)#g" \
+			-e "s#/usr/.*lib/sems/plug-in/#$(modules_target)#g" \
+			-e "s#/usr/.*etc/sems/#$(app_cfg_target)#g" \
+			< etc/$$r > $(DESTDIR)$(app_cfg_target)$$r; \
 	done
 
-$(DESTDIR)(audio-prefix)/$(audio-dir):
-	mkdir -p $(DESTDIR)$(audio-prefix)/$(audio-dir)
+$(DESTDIR)(audio_prefix)/$(audio_dir):
+	mkdir -p $(DESTDIR)$(audio_prefix)/$(audio_dir)
 
 .PHONY: $(NAME)_audio
-$(NAME)_audio: $(DESTDIR)$(audio-prefix)/$(audio-dir)
-	mkdir -p $(DESTDIR)$(audio-prefix)/$(audio-dir)$(NAME)
+$(NAME)_audio: $(DESTDIR)$(audio_prefix)/$(audio_dir)
+	mkdir -p $(DESTDIR)$(audio_prefix)/$(audio_dir)$(NAME)
 	-@for f in $(AUDIO_FILES) ; do \
 		if [ -n "wav/$$f" ]; then \
-			$(INSTALL-TOUCH) $(DESTDIR)$(audio-prefix)/$(audio-dir)$(NAME)/$$f; \
-			$(INSTALL-AUDIO) wav/$$f $(DESTDIR)$(audio-prefix)/$(audio-dir)$(NAME)/$$f; \
+			$(INSTALL_TOUCH) $(DESTDIR)$(audio_prefix)/$(audio_dir)$(NAME)/$$f; \
+			$(INSTALL_AUDIO) wav/$$f $(DESTDIR)$(audio_prefix)/$(audio_dir)$(NAME)/$$f; \
 		fi ; \
 	done
 

pkg/rpm/sems.spec
@@ -112,16 +112,16 @@ make install TTS="y" exclude_modules="examples %{!?with_ilbc:ilbc} mp3" \
 	DESTDIR=$RPM_BUILD_ROOT \
 	basedir= \
 	prefix=%{_prefix} \
-	modules-prefix= \
-        modules-dir=%{_libdir}/sems/plug-in \
-        modules-target=%{_libdir}/sems/plug-in \
-	ivr-modules-dir=%{_libdir}/sems/ivr \
-	cfg-prefix= \
-	cfg-target=%{_sysconfdir}/sems/ \
-	doc-prefix= \
-	doc-dir=%{_docdir}/sems/ \
-	audio-prefix= \
-	audio-dir=%{_libdir}/sems/audio/
+	modules_prefix= \
+        modules_dir=%{_libdir}/sems/plug-in \
+        modules_target=%{_libdir}/sems/plug-in \
+	ivr-modules_dir=%{_libdir}/sems/ivr \
+	cfg_prefix= \
+	cfg_target=%{_sysconfdir}/sems/ \
+	doc_prefix= \
+	doc_dir=%{_docdir}/sems/ \
+	audio_prefix= \
+	audio_dir=%{_libdir}/sems/audio/
 
 install -D -p -m755 pkg/rpm/sems.init $RPM_BUILD_ROOT/%{_sysconfdir}/init.d/sems
 

tools/Makefile
@@ -9,12 +9,12 @@ install: $(sems-tools) install_tools
 
 make_tools: $(sems-tools)
 
-install_tools: $(DESTDIR)$(bin-prefix)/$(bin-dir)
+install_tools: $(DESTDIR)$(bin_prefix)/$(bin_dir)
 	-@for r in $(sems-tools)  ; do \
 		x=`echo $$r | sed s/sems-/$(APP_NAME)-/g` ; \
 		echo "installing $$r -> $$x" ; \
-		$(INSTALL-TOUCH) $(DESTDIR)$(bin-prefix)/$(bin-dir)/$$x ; \
-		$(INSTALL-BIN) $$r $(DESTDIR)$(bin-prefix)/$(bin-dir)/$$x ; \
+		$(INSTALL_TOUCH) $(DESTDIR)$(bin_prefix)/$(bin_dir)/$$x ; \
+		$(INSTALL_BIN) $$r $(DESTDIR)$(bin_prefix)/$(bin_dir)/$$x ; \
 	done
 
 sems-logfile-callextract: logfile-splitter.o

Makefile
@@ -43,7 +43,7 @@ install:
 	@echo ""
 	@echo "*** install complete. Run SEMS with "
 	@echo "*** "
-	@echo "***   $(bin-target)$(NAME) -f $(cfg-target)sems.conf"
+	@echo "***   $(bin_target)$(NAME) -f $(cfg_target)sems.conf"
 
 
 .PHONY: dist

apps/Makefile
@@ -83,7 +83,7 @@ install-bin:
 	done
 
 .PHONY: install-cfg
-install-cfg: $(DESTDIR)$(cfg-target)
+install-cfg: $(DESTDIR)$(cfg_target)
 	-@for r in $(app_modules) "" ; do \
 		if [ -n "$$r" ]; then \
 			echo "" ; \

apps/dsm/mods/Makefile
@@ -58,7 +58,7 @@ install-bin:
 	done
 
 .PHONY: install-cfg
-install-cfg: $(DESTDIR)$(cfg-target)
+install-cfg: $(DESTDIR)$(cfg_target)
 	-@for r in $(dsm_modules) "" ; do \
 		if [ -n "$$r" ]; then \
 			echo "" ; \

apps/dsm/tools/Makefile
@@ -6,12 +6,12 @@ monitoring_scripts = $(wildcard sems-*)
 all: install_tools
 install: install_tools
 
-install_tools: $(DESTDIR)$(bin-prefix)/$(bin-dir)
+install_tools: $(DESTDIR)$(bin_prefix)/$(bin_dir)
 	-@for r in $(monitoring_scripts)  ; do \
 		x=`echo $$r | sed s/sems-/$(APP_NAME)-/g` ; \
 		echo "installing $$r -> $$x" ; \
-		$(INSTALL-TOUCH) $(DESTDIR)$(bin-prefix)/$(bin-dir)/$$x ; \
-		$(INSTALL-BIN) $$r $(DESTDIR)$(bin-prefix)/$(bin-dir)/$$x ; \
+		$(INSTALL_TOUCH) $(DESTDIR)$(bin_prefix)/$(bin_dir)/$$x ; \
+		$(INSTALL_BIN) $$r $(DESTDIR)$(bin_prefix)/$(bin_dir)/$$x ; \
 	done
 
 

apps/examples/Makefile
@@ -46,7 +46,7 @@ install-bin:
 	done
 
 .PHONY: install-cfg
-install-cfg: $(DESTDIR)$(cfg-target)
+install-cfg: $(DESTDIR)$(cfg_target)
 	-@for r in $(modules) "" ; do \
 		if [ -n "$$r" ]; then \
 			echo "" ; \

commit c9f8fb0f70485147e3dea204e164f82c37ff96c2

Rename remaining make variables to not use dash in their names
Fixes: commit d50b51821706f6e86547cced03bd78119ee1da61

apps/ivr/Makefile.ivr_application
@@ -10,9 +10,9 @@ include $(COREPATH)/../Makefile.defs
 include $(IVRPATH)/Makefile.defs
 
 # 
-ivr-modules_dir?=lib/$(APP_NAME)/ivr
+ivr_modules_dir?=lib/$(APP_NAME)/ivr
 
-LIB_INSTALLDIR?=$(modules_prefix)/$(ivr-modules_dir)
+LIB_INSTALLDIR?=$(modules_prefix)/$(ivr_modules_dir)
 TARBALL_PREFIX=sems-app-${NAME}
 TARBALL="${TARBALL_PREFIX}-${VERSION}.tar.gz"
 

pkg/rpm/sems.spec
@@ -115,7 +115,7 @@ make install TTS="y" exclude_modules="examples %{!?with_ilbc:ilbc} mp3" \
 	modules_prefix= \
         modules_dir=%{_libdir}/sems/plug-in \
         modules_target=%{_libdir}/sems/plug-in \
-	ivr-modules_dir=%{_libdir}/sems/ivr \
+	ivr_modules_dir=%{_libdir}/sems/ivr \
 	cfg_prefix= \
 	cfg_target=%{_sysconfdir}/sems/ \
 	doc_prefix= \

tools/Makefile
@@ -3,14 +3,14 @@ all: make_tools
 COREPATH ?= ../core
 include $(COREPATH)/../Makefile.defs
 
-sems-tools = sems-logfile-callextract
+sems_tools = sems-logfile-callextract
 
-install: $(sems-tools) install_tools
+install: $(sems_tools) install_tools
 
-make_tools: $(sems-tools)
+make_tools: $(sems_tools)
 
 install_tools: $(DESTDIR)$(bin_prefix)/$(bin_dir)
-	-@for r in $(sems-tools)  ; do \
+	-@for r in $(sems_tools)  ; do \
 		x=`echo $$r | sed s/sems-/$(APP_NAME)-/g` ; \
 		echo "installing $$r -> $$x" ; \
 		$(INSTALL_TOUCH) $(DESTDIR)$(bin_prefix)/$(bin_dir)/$$x ; \