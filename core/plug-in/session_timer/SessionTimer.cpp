7ea2f1c75a111f59a1be7dcfd1aea657f5e0c625
Juha Heinanen <jh@tutpro.com>
2017-08-29 14:46:48 +0300

    core/plug-in/session_timer: fixed sending of new INVITE after 422 reply - after 422 Session Timer Too Small reply, original INVITE must be   re-send in a new transaction with larger Session-Expires value and   increased CSeq value

--- core/plug-in/session_timer/SessionTimer.cpp
+++ core/plug-in/session_timer/SessionTimer.cpp
@@ -89,46 +89,62 @@ bool SessionTimer::onSipReply(const AmSipRequest& req, const AmSipReply& reply,
 			      AmBasicSipDialog::Status old_dlg_status)
 {
   if (session_timer_conf.getEnableSessionTimer() &&
-      (reply.code == 422) &&
       ((reply.cseq_method == SIP_METH_INVITE) || 
        (reply.cseq_method == SIP_METH_UPDATE))) {
-
-    // get Min-SE
-    unsigned int i_minse;
-    string min_se_hdr = getHeader(reply.hdrs, SIP_HDR_MIN_SE, true);
-    if (!min_se_hdr.empty()) {
-      if (str2i(strip_header_params(min_se_hdr), i_minse)) {
-	WARN("error while parsing " SIP_HDR_MIN_SE " header value '%s'\n",
-	     strip_header_params(min_se_hdr).c_str());
-      } else {
-	
-	if (i_minse <= session_timer_conf.getMaximumTimer()) {
-	  session_interval = i_minse;
-	  unsigned int new_cseq = s->dlg->cseq;
-	  // resend request with interval i_minse
-	  if (s->dlg->sendRequest(req.method, &req.body,req.hdrs) == 0) {
-	    DBG("request with new Session Interval %u successfully sent.\n", i_minse);
-	    // undo SIP dialog status change
-	    if (s->dlg->getStatus() != old_dlg_status)
-	      s->dlg->setStatus(old_dlg_status);
-
-	    s->updateUACTransCSeq(reply.cseq, new_cseq);
-	    // processed
-	    return true;
-	  } else {
-	    ERROR("failed to send request with new Session Interval.\n");
-	  }
+    if (reply.code == 422) {
+      // get Min-SE
+      unsigned int i_minse;
+      string min_se_hdr = getHeader(reply.hdrs, SIP_HDR_MIN_SE, true);
+      if (!min_se_hdr.empty()) {
+	if (str2i(strip_header_params(min_se_hdr), i_minse)) {
+	  WARN("error while parsing " SIP_HDR_MIN_SE " header value '%s'\n",
+	       strip_header_params(min_se_hdr).c_str());
 	} else {
-	  DBG("other side requests too high Min-SE: %u (our limit %u)\n",
-	      i_minse, session_timer_conf.getMaximumTimer());
+	  if (i_minse <= session_timer_conf.getMaximumTimer()) {
+	    session_interval = i_minse;
+	    unsigned int new_cseq = s->dlg->cseq;
+	    DBG("old dialog status is: %s\n", AmBasicSipDialog::getStatusStr(old_dlg_status));
+	    // resend request with interval i_minse
+	    std::map<unsigned int, SIPRequestInfo>::iterator ri = 
+	      sent_requests.find(reply.cseq);
+	    if (ri != sent_requests.end()) {
+	      s->dlg->setRemoteTag("");
+	      if (s->dlg->sendRequest(req.method, &(ri->second.body),
+				      ri->second.hdrs) == 0) {
+		DBG("request with new Session Interval %u successfully sent\n", i_minse);
+		// undo SIP dialog status change
+		DBG("dialog status is: %s\n", s->dlg->getStatusStr());
+		if (s->dlg->getStatus() != old_dlg_status)
+		  s->dlg->setStatus(old_dlg_status);
+		s->updateUACTransCSeq(reply.cseq, new_cseq);
+
+		// processed
+		DBG("erasing %d from sent_requests\n", req.cseq);
+		sent_requests.erase(reply.cseq);
+		return true;
+	      } else {
+		ERROR("failed to send request with new Session Interval\n");
+	      }
+	    } else {
+	      ERROR("could not find sent request with cseq %u\n", reply.cseq);
+	    }
+	  } else {
+	    DBG("other side requests too high Min-SE: %u (our limit %u)\n",
+		i_minse, session_timer_conf.getMaximumTimer());
+	  }
 	}
+      } else {
+	WARN("mandatory Min-SE header missing in 422 reply\n");
       }
     }
-  } 
-
+    if (reply.code >= 200) {
+      DBG("erasing %d from sent_requests\n", req.cseq);
+      sent_requests.erase(reply.cseq);
+    }
+  }
+  
   if ((reply.cseq_method == SIP_METH_INVITE) || 
       (reply.cseq_method == SIP_METH_UPDATE)) {
-
     updateTimer(s,reply);
   }
 
@@ -142,14 +158,12 @@ bool SessionTimer::onSendRequest(AmSipRequest& req, int& flags)
     return false;
   }
 
-  // if (session_timer_conf.getEnableSessionTimer() &&
-  //     ((req.method == SIP_METH_INVITE) || (req.method == SIP_METH_UPDATE))) {
+  if (session_timer_conf.getEnableSessionTimer() &&
+      ((req.method == SIP_METH_INVITE) || (req.method == SIP_METH_UPDATE))) {
     // save INVITE and UPDATE so we can resend on 422 reply
-    // DBG("adding %d to list of sent requests.\n", req.cseq);
-    // sent_requests[req.cseq] = SIPRequestInfo(req.method,
-    // 					     &req.body,
-    // 					     req.hdrs);
-  // }
+    DBG("adding %d to sent_requests\n", req.cseq);
+    sent_requests[req.cseq] = SIPRequestInfo(req.method, &req.body, req.hdrs);
+  }
 
   addOptionTag(req.hdrs, SIP_HDR_SUPPORTED, TIMER_OPTION_TAG);
   if  ((req.method != SIP_METH_INVITE) && (req.method != SIP_METH_UPDATE))
--- core/plug-in/session_timer/SessionTimer.h
+++ core/plug-in/session_timer/SessionTimer.h
@@ -120,7 +120,7 @@ protected:
   AmSession* s;
 
   // map to save sent requests, so we can resent in case of 422
-  //std::map<unsigned int, SIPRequestInfo> sent_requests;
+  std::map<unsigned int, SIPRequestInfo> sent_requests;
 
   enum SessionRefresher {
     refresh_local,
