91310d5ce2fa036238c8066675d5b53fa4445f4f
Peter Lemenkov <lemenkov@gmail.com>
2019-09-01 23:25:54 +0200

    Remove cmake templates from the tree
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- apps/announce_transfer/etc/announce_transfer.conf.cmake
+++
@@ -1,7 +0,0 @@
-#CFGOPTION_SEMS_ANNOUNCEPATH
-announce_path=${SEMS_AUDIO_PREFIX}/sems/audio/
-#ENDCFGOPTION
-
-#CFGOPTION_SEMS_ANNOUNCEMENT
-default_announce=default_en.wav
-#ENDCFGOPTION
--- apps/msg_storage/etc/msg_storage.conf.cmake
+++
@@ -1,8 +0,0 @@
-
-#
-# storage_dir is where messages are saved on disk. this 
-# folder must be writable. 
-#
-# default: /var/spool/voicebox/
-#
-#storage_dir=/var/spool/voicebox/
