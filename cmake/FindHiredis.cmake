0243af51be74e23ca846bbde6c841c309d5e8583
Marcel Hecko <marcel.hecko@moirelabs.com>
2025-10-27 22:58:26 +0100

    Apply clang-format

--- cmake/FindHiredis.cmake
+++ cmake/FindHiredis.cmake
@@ -1,9 +1,7 @@
-# Try to find hiredis
-# Once done, this will define
+# Try to find hiredis Once done, this will define
 #
-# HIREDIS_FOUND        - system has hiredis
-# HIREDIS_INCLUDE_DIRS - hiredis include directories
-# HIREDIS_LIBRARIES    - libraries need to use hiredis
+# HIREDIS_FOUND        - system has hiredis HIREDIS_INCLUDE_DIRS - hiredis
+# include directories HIREDIS_LIBRARIES    - libraries need to use hiredis
 
 if(HIREDIS_INCLUDE_DIRS AND HIREDIS_LIBRARIES)
   set(HIREDIS_FIND_QUIETLY TRUE)
@@ -23,9 +21,9 @@ else()
   set(HIREDIS_INCLUDE_DIRS ${HIREDIS_INCLUDE_DIR})
   set(HIREDIS_LIBRARIES ${HIREDIS_LIBRARY})
 
-  include (FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(
-    Hiredis DEFAULT_MSG HIREDIS_LIBRARY HIREDIS_INCLUDE_DIR)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Hiredis DEFAULT_MSG HIREDIS_LIBRARY
+                                    HIREDIS_INCLUDE_DIR)
 
   mark_as_advanced(HIREDIS_LIBRARY HIREDIS_INCLUDE_DIR)
 endif()
